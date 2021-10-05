13f70336a7428a2fac9ed664457425f381448fab
Ross Farrugia <82581364+rossfarrugia@users.noreply.github.com>
2021-10-05 13:23:10 +0100

    Create advanced_exercise.R

---
+++ Hands_on_Exercises/Advanced/advanced_exercise.R
@@ -0,0 +1,22 @@
+# -----------------------------------------------------------------------------------
+# Hands-on Exercise: Advanced 
+# (Best for those with 3+ years of R and ADaM experience)
+#  
+# See "ExerciseSheet_advanced.doc" for all exercise questions, and if you get stuck ask for help from one of our facilitators or consult "SolutionSheet_advanced.doc"
+# 
+# This program is to be used as a starter template as you work through the exercises
+# -----------------------------------------------------------------------------------
+
+# Load all required packages
+
+library(admiral)
+library(dplyr)
+library(lubridate)
+library(stringr)
+library(haven)
+
+# Read in the input data from Github using haven
+
+advs_temp <- read_xpt(url("https://github.com/Roche-GSK/admiral.phuse.workshop/raw/main/Hands_on_Exercises/advs_temp.xpt"))
+
+# Use admiral functions to create the required baseline variables
