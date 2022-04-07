6ffd9ad1db3e6f35763e37e59feed8317581e02e
Alice Ehmann <alice.x.ehmann@gsk.com>
2022-04-07 13:09:22 -0400

    Remove ADSL exercise

--- US 2022/admiral_exercises/Beginner/beginner_exercise.R
+++ US 2022/admiral_exercises/Beginner/beginner_exercise.R
@@ -16,7 +16,6 @@ library(haven)
 
 # Read in the input data from Github using haven
 advs_temp <- read_xpt(url("https://github.com/pharmaverse/admiral.phuse.workshop/raw/main/US%202022/data/advs_temp.xpt"))
-adsl_admiral <- read_xpt(url("https://github.com/pharmaverse/admiral.phuse.workshop/raw/main/US%202022/data/adsl_admiral.xpt"))
 
 # Run the following code to create the required baseline variables
 
@@ -41,12 +40,3 @@ advs <- advs_temp %>%
 
   # Sort the data frame
   arrange(USUBJID, PARAMCD, ADT)
-
-# Run the following code to derive EMA and FDA age groups
-
-adsl <- adsl_admiral %>%
-  select(STUDYID, USUBJID, AGE, AGEU) %>%
-
-  # Calculate EMA age group
-  derive_var_agegr_ema(AGE, age_unit = "years", AGEGR1)
-
